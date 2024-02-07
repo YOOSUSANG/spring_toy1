020c1ca3bad2f8a4b15ed5f3f9b3362236f5eb9a
yoosusang <dbtntkd456@naver.com>
2024-02-07 16:19:28 +0900

    feat: 1. 주문 레포지토리 작성 2. 커스텀 레포지토리 작성  2.1 search(검색 조건DTO, 유저메일)

--- project/src/main/java/family/project/repository/OrderRepository.java
+++ project/src/main/java/family/project/repository/OrderRepository.java
@@ -3,14 +3,15 @@ package family.project.repository;
 import family.project.domain.Order;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
+public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositorySearchCustom {
 
 
     @Override
@@ -18,4 +19,12 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Override
     List<Order> findAll(Sort sort);
+
+    @Query("select o from Order o" +
+            " join fetch o.member m" +
+            " join fetch o.delivery d" +
+            " where m.email = :email")
+    List<Order> findByMyOrders(@Param("email") String email);
+
+
 }
